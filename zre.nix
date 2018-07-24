4d0310bcc6b68840ab1d5e4ec064dba535dd80ee
Richard Marko <srk@48.io>
2018-07-24 18:14:31 +0200

    nix updates

--- zre.nix
+++ zre.nix
@@ -1,25 +1,27 @@
 { mkDerivation, async, attoparsec, base, binary, binary-strict
-, bytestring, containers, lifted-async, monad-control, mtl, network
-, network-info, network-multicast, optparse-applicative, process
-, random, sockaddr, stdenv, stm, time, transformers-base, uuid
-, zeromq4-haskell
+, bytestring, config-ini, containers, data-default, lifted-async
+, monad-control, mtl, network, network-info, network-multicast
+, optparse-applicative, process, random, repline, sockaddr, stdenv
+, stm, text, time, transformers-base, uuid, zeromq4-haskell
 }:
 mkDerivation {
   pname = "zre";
-  version = "0.1.0.0";
-  sha256 = "11lnz7pxmqz39xjqjh1kkgywv0jg81yzi2hrp2ibaw2nslf65xzl";
+  version = "0.1.0.1";
+  sha256 = "0ddd7ppx8ivgrcvaj1d74vxjrgly8y5k5yr181dmi0z0jfkkkfx4";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    async attoparsec base binary binary-strict bytestring containers
-    monad-control mtl network network-info network-multicast
-    optparse-applicative process random sockaddr stm time
-    transformers-base uuid zeromq4-haskell
+    async attoparsec base binary binary-strict bytestring config-ini
+    containers data-default monad-control mtl network network-info
+    network-multicast optparse-applicative process random sockaddr stm
+    text time transformers-base uuid zeromq4-haskell
   ];
   executableHaskellDepends = [
-    async base bytestring lifted-async monad-control mtl stm time
+    async base bytestring lifted-async monad-control mtl repline stm
+    time
   ];
   testHaskellDepends = [ base ];
+  homepage = "https://github.com/vpsfreecz/haskell-zre/";
   description = "ZRE protocol implementation";
   license = stdenv.lib.licenses.bsd3;
 }
