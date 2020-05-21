991be733aae8aea61788165f6c58c5d6d9da1e4d
Richard Marko <srk@48.io>
2020-05-21 17:19:17 +0200

    refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -39,30 +39,37 @@ main = runZre replApp
 
 replApp :: ZRE ()
 replApp = void $ do
-      recv `concurrently` repl
-      where
-        recv = forever $ do
-          evt <- readZ
-          case evt of
-            New uuid mname groups headers endpoint -> put ["New peer", toASCIIBytes uuid, pEndpoint endpoint]
-            Ready uuid name groups headers endpoint -> put ["Ready peer", name, toASCIIBytes uuid]
-            Quit uuid mname -> put ["Peer quit", toASCIIBytes uuid]
-            GroupJoin uuid group -> put ["Join group", unGroup group, toASCIIBytes uuid]
-            GroupLeave uuid group -> put ["Leave group", unGroup group, toASCIIBytes uuid]
-            Shout _uuid group content _time -> put ["Shout for group", unGroup group, ">", B.concat content]
-            Whisper uuid content _time -> put ["Whisper from", toASCIIBytes uuid, B.concat content]
-            x -> liftIO $ print x
+  recv `concurrently` repl
+  where
+    recv = forever $ do
+      evt <- readZ
+      case evt of
+        New uuid mname groups headers endpoint ->
+          put ["New peer", toASCIIBytes uuid, pEndpoint endpoint]
+        Ready uuid name groups headers endpoint ->
+          put ["Ready peer", name, toASCIIBytes uuid]
+        Quit uuid mname ->
+          put ["Peer quit", toASCIIBytes uuid]
+        GroupJoin uuid group ->
+          put ["Join group", unGroup group, toASCIIBytes uuid]
+        GroupLeave uuid group ->
+          put ["Leave group", unGroup group, toASCIIBytes uuid]
+        Shout _uuid group content _time ->
+          put ["Shout for group", unGroup group, ">", B.concat content]
+        Whisper uuid content _time ->
+          put ["Whisper from", toASCIIBytes uuid, B.concat content]
+        x -> liftIO $ print x
 
-        repl = do
-          q <- getApiQueue
-          liftIO $ evalRepl (pure ">>> ") (cmd q) [] Nothing (Word completer) ini
-          liftIO $ atomically $ writeTBQueue q DoQuit
+    repl = do
+      q <- getApiQueue
+      liftIO $ evalRepl (pure ">>> ") (cmd q) [] Nothing (Word completer) ini
+      liftIO $ atomically $ writeTBQueue q DoQuit
 
-        cmd :: APIQueue -> String -> Repl ()
-        cmd q x = do
-          case parseAttoApi $ B.pack x of
-            (Left err) -> liftIO $ B.putStrLn $ B.pack $ "Unable to parse command: " ++ err
-            (Right cmd) -> liftIO $ atomically $ writeTBQueue q cmd
-          return ()
+    cmd :: APIQueue -> String -> Repl ()
+    cmd q x = do
+      case parseAttoApi $ B.pack x of
+        (Left err) -> liftIO $ B.putStrLn $ B.pack $ "Unable to parse command: " ++ err
+        (Right cmd) -> liftIO $ atomically $ writeTBQueue q cmd
+      return ()
 
-        put = liftIO . B.putStrLn . (B.intercalate " ")
+    put = liftIO . B.putStrLn . (B.intercalate " ")
